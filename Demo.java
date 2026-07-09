5884e4139549b038cec5972b0aa7db7bf741854a
vihangadilsara <vdilsara30@gmail.com.com>
2026-07-09 15:27:18 +0530

    refactor: simplify main method by removing unused input and max calculations

--- Demo.java
+++ Demo.java
@@ -1,37 +1,13 @@
 import java.util.*;
 class Example{
 	public static void main(String args[]){
-		Scanner input = new Scanner(System.in);
-		System.out.print("Input a: ");
-		int a = input.nextInt();
-		System.out.print("Input b: ");
-		int b = input.nextInt();
-		System.out.print("Input c: ");
-		int c = input.nextInt();
-		System.out.print("Input d: ");
-		int d = input.nextInt();
-		System.out.print("Input e: ");
-		int e = input.nextInt();
+		int y = 100;
+		int x;
 		
-		int max = a;
-		
-		if(max < b) {
-			max = b;
-		}
-		// checked a, b 
-		
-		if(max < c) {
-			max = c;
+		if(y == 100) { // Illegal
+			x = 200;
 		}
 		
-		if(max < d) {
-			max = d;
-		}
-		
-		if(max < e) {
-			max = e;
-		}
-		
-		System.out.println("Maximum Value: " + max);
+		System.out.println(x);
 	}
 }

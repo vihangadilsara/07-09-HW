0f6e9772a594dbb219943a1a45e3396d2202af36
vihangadilsara <vdilsara30@gmail.com.com>
2026-07-10 01:10:54 +0530

    fix: correct variable initialization and update conditional logic

--- Demo.java
+++ Demo.java
@@ -1,16 +1,13 @@
 import java.util.*;
 class Example{
 	public static void main(String args[]){
-		int y = 100;
-		int x;
-		
-		if(y == 100) {
-			x = 200;
+		int x = 99;
+		int y, z;
+		if(x > 100) { // Cannot use Turnery Operator
+			y = 100;
 		} else {
-			
-		}
-
-		
-		System.out.println(x);
+			z = 200;
+		}		
+		System.out.println("x: " + x + " | y: " + y); // x:  | y: 
 	}
 }
